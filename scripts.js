b15a428ab50891b58609d223da1d43d1a7c52f1a
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2018-01-04 11:44:54 -0500

    Add a module to hold the controller, refactor controller

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
-var MainController = function($scope, $http) {
-    $http.get("https://api.github.com/users/andrewniekamp")
-    .then( (res) => $scope.user = res.data)
-};
+(function() {
+    
+    const app = angular.module("githubViewer", []);
+
+    const MainController = function($scope, $http) {
+        const onUserComplete = (res) => {
+            $scope.user = res.data;
+        }
+        const onError = (reason) => {
+            $scope.error = "Could not fetch the user";
+        }
+        $http.get("https://api.github.com/users/andrewniekamp")
+        .then(onUserComplete, onError);
+    };
+
+    app.controller("MainController", MainController); // Not typical
+
+}());
+
